367fb05110a36f7e327bb651f33a435780d483ca
CupaState <olegivanco@yandex.ru>
2020-10-15 02:53:02 +0300

    EasterEgg

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import './App.css';
 import NewHeader from "./Components/NewHeader";
 import Footer from "./Components/Footer";
 
-
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const Sakura = lazy(()=>import("./Components/Sakura"));
 
 class App extends Component{
   constructor(props)
   {
     super(props);
+    this.state = {
+      isOpenSakura: false,
+    }
+    this.openSakura = this.openSakura.bind(this);
+    this.closeSakura = this.closeSakura.bind(this);
+  }
+
+  openSakura()
+  {
+    this.setState({ isOpenSakura: true});
+    const app = document.getElementById("app-wrapper");
+    app.classList.add("hide");
+  }
+
+  closeSakura()
+  {
+    this.setState({ isOpenSakura: false })
+    const app = document.getElementById("app-wrapper");
+    app.classList.remove("hide");
   }
 
   render()
@@ -27,10 +47,16 @@ class App extends Component{
           <span>K</span>
           <span>A</span>
         </div>
-      <div className="App" id="app-wrapper">
-        <NewHeader/>
-        <Footer/>
-      </div>
+        <div className="App" id="app-wrapper">
+          <NewHeader/>
+          <Footer openSakura={ this.openSakura }/>
+        </div>
+        <Suspense fallback={<div></div>}>
+          <Sakura
+            isOpenSakura={ this.state.isOpenSakura }
+            closeSakura={ this.closeSakura }
+          />
+        </Suspense>
       </div>
 
     );
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,65 +7,76 @@ import whatsapp from "../assets/icons/footerIcons/whatsapp.ico";
 import "../css/Footer.css";
 
 export default class Footer extends Component {
-constructor(props)
-{
-    super(props);
-    
-}
 
     render() {
         return (
             <>
-            <div className="footer-block">
+            <div
+                className="footer-block"
+            >
                 <a className="footer-icon-vk"
                     href="https://vk.com/arttiger_catalog"
                     target="_blank"
+                    rel="noopener noreferrer"
                 >
                     <img
                         src={vk}
                         className="footer-icon"
+                        alt="VK"
                     />
                 </a>
                 <a className="footer-icon-fb"
                     href="https://www.facebook.com/groups/1157346364376526/"
                     target="_blank"
+                    rel="noopener noreferrer"
                 >
                     <img
                         src={facebook}
                         className="footer-icon"
+                        alt="Facebook"
                     />
                 </a>
                 <a className="footer-icon-yt"
                     href="https://www.youtube.com/channel/UCuBoNl2xm6HlkZLc9qR-9TA"
                     target="_blank"
+                    rel="noopener noreferrer"
                 >
                     <img
                         src={youtoube}
                         className="footer-icon"
+                        alt="YouTube"
                     />
                 </a>
                 <a className="footer-icon-inst"
                     href="https://www.instagram.com/art_tiger_/"
                     target="_blank"
+                    rel="noopener noreferrer"
                 >
                     <img 
                         src={instagram}
                         className="footer-icon"
+                        alt="Instagram"
                     />
                 </a>
                 <a className="footer-icon-whatsapp"
                     href="https://wa.me/79776913936?text=Здравствуйте.%20Меня%20интересуют%20услуги%20Вашей%20компании"
                     target="_blank"
+                    rel="noopener noreferrer"
                     onClick={() => {window.open('https://wa.me/79776913936?text=Здравствуйте.%20Меня%20интересуют%20услуги%20Вашей%20компании')}}
 
                 >
                     <img 
                         src={whatsapp}
                         className="footer-icon"
+                        alt="WhatsApp"
                     />
                 </a>
             </div>
-            <div className="artica-bottom">
+            <div
+                className="artica-bottom"
+                onClick={ this.props.openSakura }
+                title="Пасхалка! Жми!"
+            >
                 <span className="artica-bottom-text">
                     &#169; 2010-2020 "АРТИКА" - творческое объединение художников, г. Москва и МО
                 </span>
